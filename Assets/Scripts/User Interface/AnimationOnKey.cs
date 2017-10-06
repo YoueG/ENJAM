5537eb7bb222f5dc94d78d979d77e9b050af9e48
YoueG <Youe.Graillot@etu.udamail.fr>
2017-10-07 00:03:07 +0200

    Anim tuto

---
+++ Assets/Scripts/User Interface/AnimationOnKey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationOnKey : MonoBehaviour {
+	[SerializeField]
+	KeyCode m_key;
+
+	Animator m_animator;
+
+	bool m_activated = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(m_key) && !m_activated)
+		{
+			m_activated = true;
+			m_animator.SetTrigger("Disappear");
+		}
+	}
+}
