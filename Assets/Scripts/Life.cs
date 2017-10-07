597c872af62f1a5139486c5fb8ebda209217b159
YoueG <Youe.Graillot@etu.udamail.fr>
2017-10-07 15:38:26 +0200

    Dones

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+	[SerializeField, Range(0,1)]
+	float m_life = 1;
+
+	[SerializeField]
+	MeshRenderer m_jaugeRenderer;
+	Material m_jaugeMat;
+
+	void Start () {
+		m_jaugeMat = m_jaugeRenderer.material;
+	}
+	
+	void Update ()
+	{
+		m_jaugeMat.SetFloat("_Ratio", m_life);
+	}
+}
