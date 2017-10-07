13030980dae83bd3d7a04f492164ce78db88b753
YoueG <Youe.Graillot@etu.udamail.fr>
2017-10-08 00:34:42 +0200

    Maj ennemy

---
+++ Assets/Scripts/Gameplay/Ennemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+	[SerializeField]
+	Transform m_weaponParent;
+	[SerializeField]
+	Transform m_buste;
+	[SerializeField]
+	Animator m_animator;
+
+	void Start()
+	{
+		m_buste.transform.localScale = new Vector3(Random.Range(0, 2) == 1 ? -1 : 1, 1, 1);
+
+		m_weaponParent.GetChild(Random.Range(0, m_weaponParent.childCount)).gameObject.SetActive(true);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.CompareTag("Projectile"))
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		GetComponent<NavMeshAgent>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+		m_animator.SetBool("Dead", true);
+	}
+}
