303482e321e5f1d02f89682ba3b9df0014b4f327
Aredhele <vincentstehly@hotmail.fr>
2017-10-08 12:45:36 +0200

    Added menu script and dummy graphics

---
+++ Assets/Scripts/User Interface/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Load the level
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // Exit the Game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
